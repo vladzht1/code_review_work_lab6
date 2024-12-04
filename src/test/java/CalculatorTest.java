e8b1e71ae39dcd2408207f6c406a0298233a620d
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2024-12-04 23:22:45 +0300

    add tests for calculator

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,29 +3,42 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    private Calculator calculator;
+
+    public CalculatorTest() {
+        calculator = new Calculator();
+    }
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        assert(calculator.add(2, 3) == 5);
+        assert(calculator.add(-4, 4) == 0);
+        assert(calculator.add(1, 0) == 1);
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        assert(calculator.dif(10, 5) == 5);
+        assert(calculator.dif(3, 6) == -3);
+        assert(calculator.dif(4, 4) == 0);
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        assert(calculator.div(10, 5) == 5);
+        assert(calculator.div(2, 2) == 1);
+        assertThrows(ArithmeticException.class, () -> calculator.div(2, 0));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        assert(calculator.times(2, 2) == 4);
+        assert(calculator.times(5, 0) == 0);
+        assert(calculator.times(-2, 7) == -14);
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        assert(calculator.solver() == 14);
     }
 }
