cf6db0fa0fa056a578816e67a28ae9698948e258
Vladislav Zheltov <vlad.zheltov1@gmail.com>
2024-12-04 23:46:57 +0300

    fix tests: `assert` -> `assertEquals`

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -11,34 +11,34 @@ class CalculatorTest {
 
     @Test
     void add() {
-        assert(calculator.add(2, 3) == 5);
-        assert(calculator.add(-4, 4) == 0);
-        assert(calculator.add(1, 0) == 1);
+        assertEquals(calculator.add(2, 3), 5);
+        assertEquals(calculator.add(-4, 4), 0);
+        assertEquals(calculator.add(1, 0), 1);
     }
 
     @Test
     void dif() {
-        assert(calculator.dif(10, 5) == 5);
-        assert(calculator.dif(3, 6) == -3);
-        assert(calculator.dif(4, 4) == 0);
+        assertEquals(calculator.dif(10, 5), 5);
+        assertEquals(calculator.dif(3, 6), -3);
+        assertEquals(calculator.dif(4, 4), 0);
     }
 
     @Test
     void div() {
-        assert(calculator.div(10, 5) == 5);
-        assert(calculator.div(2, 2) == 1);
+        assertEquals(calculator.div(10, 5), 5);
+        assertEquals(calculator.div(2, 2), 1);
         assertThrows(ArithmeticException.class, () -> calculator.div(2, 0));
     }
 
     @Test
     void times() {
-        assert(calculator.times(2, 2) == 4);
-        assert(calculator.times(5, 0) == 0);
-        assert(calculator.times(-2, 7) == -14);
+        assertEquals(calculator.times(2, 2), 4);
+        assertEquals(calculator.times(5, 0), 0);
+        assertEquals(calculator.times(-2, 7), -14);
     }
 
     @Test
     void solver() {
-        assert(calculator.solver() == 14);
+        assertEquals(calculator.solver(), 14);
     }
 }
